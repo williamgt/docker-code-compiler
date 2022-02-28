2c75e374628d56b3f242a530078da17092c5e7bf
William G. Tresselt <williagt@stud.ntnu.no>
2022-02-28 11:37:51 +0100

    prepped for real implementation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CodeCompileView></CodeCompileView>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CodeCompileView from '@/components/CodeCompileView'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CodeCompileView
   }
 }
 </script>
---
+++ src/components/CodeCompileView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="compiler-container">
+    <h1>Please enter your code:</h1>
+    <textarea v-model="code" id="code-input"></textarea>
+    <button id="compile-button" @click="dummyCompile">Compile</button>
+    <h1>Output:</h1>
+    <p id="output">placeholder {{output}}</p>
+    <p>{{ this.code }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'CodeCompileView',
+  data () {
+    return {
+      code: '',
+      output: ''
+    }
+  },
+  methods: {
+    compile () {
+      console.log('Compiling')
+      // write code to file
+      // run container and pipe output to output-file
+      // take contents of output file and put in output
+      return 0
+    },
+    dummyCompile () {
+      axios.get('http://localhost:8085/test/' + this.code)
+        .then(response => {
+          this.output = response.data
+        })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+ .compiler-container{
+   display: flex;
+   flex-direction: column;
+   gap: 10px;
+ }
+ #compile-button{
+   width: 70px;
+   align-self: center;
+ }
+ #code-input{
+   width: 400px;
+   height: 200px;
+   align-self: center;
+ }
+ #output{
+   width: 400px;
+   height: 200px;
+   align-self: center;
+   border-style: solid;
+   text-align: start;
+ }
+</style>
